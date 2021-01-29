e57e9857f93912f823f8979a59c48b7b8911bc44
Ylana Aukamp <yaukamp0928@gmail.com>
2021-01-29 18:39:29 -0500

    

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+ import logo from './logo.svg';
 import './App.css';
+import Welcome from "./Components/Welcome/Welcome"
+import Clock from './Components/Clock/Clock.js'
+import Contact from "./Components/Contact/Contact.js"
+import { Route, Switch } from "react-router-dom"
+import Navigation from './Components/Navigation/Navigation';
+import NotFound from "./Components/NotFound/NotFound";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Switch>
+      <Route 
+        exact 
+        path="/" 
+        render={ (...props) => <Welcome name="Lana" /> }
+         />
+      <Route path='/clock' component={Clock} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/welcome/:name" component={Welcome} />
+      <Route><div>404</div></Route>
+
+      </Switch>
+      
     </div>
   );
 }
